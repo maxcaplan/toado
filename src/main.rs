d9288c863609ba9dca67c1e92b83ba8bb1a49f42
maxcaplan <maxacaplan@gmail.com>
2024-04-30 12:40:58 -0300

    Refactored error propegation

--- src/main.rs
+++ src/main.rs
@@ -6,42 +6,61 @@ mod flags;
 fn main() {
     let args = flags::Cli::parse();
 
-    let app_dir = init_directory().unwrap_or_else(|e| {
-        eprintln!("Failed to initialize application directory: {e}");
-        process::exit(1)
-    });
+    let run = || -> Result<(), toado::Error> {
+        let app_dir = match init_directory() {
+            Ok(d) => d,
+            Err(e) => {
+                eprintln!("Failed to initialize application directory: {e}");
+                return Err(e);
+            }
+        };
 
-    let app = match toado::Server::open(&format!("{app_dir}/database")) {
-        Ok(app) => app,
-        Err(e) => {
-            eprintln!("Failed to create application server: {e}");
-            process::exit(1)
+        let app = match toado::Server::open(&format!("{app_dir}/database")) {
+            Ok(app) => app,
+            Err(e) => {
+                eprintln!("Failed to create application server: {e}");
+                return Err(e);
+            }
+        };
+
+        if let Err(e) = app.init() {
+            eprintln!("Failed to initialize application server: {e}");
+            return Err(e);
+        };
+
+        // If command provided, execute and exit application
+        if let Some(command) = args.command {
+            let message = match handle_command(command, app) {
+                Ok(message) => message,
+                Err(e) => {
+                    eprintln!("Failed to execute command: {e}");
+                    return Err(e);
+                }
+            };
+
+            // If command returns message, print to stdout
+            if let Some(message) = message {
+                println!("{message}")
+            }
+
+            return Ok(());
         }
+
+        // TODO: If no command provided, run TUI
+        println!("toado");
+        Ok(())
     };
 
-    app.init().unwrap_or_else(|e| {
-        eprintln!("Failed to initialize application server: {e}");
-        process::exit(1)
-    });
-
-    // If command provided, execute and exit application
-    if let Some(command) = args.command {
-        if let Some(message) = handle_command(command, app).unwrap_or_else(|e| {
-            eprintln!("Failed to execute application command: {e}");
-            process::exit(1)
-        }) {
-            // If command returned a message, print to stdout
-            println!("{message}")
+    if let Err(e) = run() {
+        if let Some(e) = e.source() {
+            eprintln!("Caused by: {e}")
         }
-
-        return;
+        process::exit(1)
     }
-
-    println!("toado");
 }
 
 /// Creates the directory for application files if one does not exist
-fn init_directory() -> Result<String, toado::Error> {
+pub fn init_directory() -> Result<String, toado::Error> {
     // Get user home directory
     let home_dir = env::var("HOME")?;
     let app_dir = format!("{home_dir}/.local/share/toado");
@@ -66,12 +85,16 @@ fn handle_command(
 }
 
 fn on_add(args: flags::AddArgs, app: toado::Server) -> Result<Option<String>, toado::Error> {
-    app.add_task(toado::AddTaskArgs {
-        name: args.name.unwrap_or("unset".to_string()),
-        priority: 0,
-        status: toado::ItemStatus::Incomplete,
-        start_time: "2024-05-05".to_string(),
-        end_time: None,
-    })
-    .map(|id| Some(format!("Created new task with id `{id}`")))
+    if args.task || !args.project {
+        app.add_task(toado::AddTaskArgs {
+            name: args.name.unwrap_or("unset".to_string()),
+            priority: 0,
+            status: toado::ItemStatus::Incomplete,
+            start_time: "2024-05-05".to_string(),
+            end_time: None,
+        })
+        .map(|id| Some(format!("Created new task with id `{id}`")))
+    } else {
+        Err(Into::into("Project adding not implemented"))
+    }
 }
