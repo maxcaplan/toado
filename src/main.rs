5afe0acd6fd281076b6412439bdbd920d803ffb4
maxcaplan <maxacaplan@gmail.com>
2024-05-01 20:03:50 -0300

    Moved application command logic to module

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 use clap::Parser;
 use std::{env, fs, process};
 
+mod commands;
 mod flags;
 
+/// "The ships hung in the sky in much the same way that bricks don't."
 fn main() {
     let args = flags::Cli::parse();
 
@@ -60,6 +62,9 @@ fn main() {
 }
 
 /// Creates the directory for application files if one does not exist
+///
+/// # Errors
+/// Will return an error if the creation of the application directories fails
 pub fn init_directory() -> Result<String, toado::Error> {
     // Get user home directory
     let home_dir = env::var("HOME")?;
@@ -71,12 +76,15 @@ pub fn init_directory() -> Result<String, toado::Error> {
 }
 
 /// Handle application commands from the CLI
+///
+/// # Errors
+/// Will return an error if the executed command fails
 fn handle_command(
     command: flags::Commands,
     app: toado::Server,
 ) -> Result<Option<String>, toado::Error> {
     let message = match command {
-        flags::Commands::Add(args) => on_add(args, app)?,
+        flags::Commands::Add(args) => handle_add(args, app)?,
         flags::Commands::Delete(_args) => return Err(Into::into("deletion is not implemented")),
         flags::Commands::Ls(_args) => return Err(Into::into("listing is not implemented")),
     };
@@ -84,112 +92,15 @@ fn handle_command(
     Ok(message)
 }
 
-fn on_add(args: flags::AddArgs, app: toado::Server) -> Result<Option<String>, toado::Error> {
+/// Handle the add command
+///
+/// # Errors
+/// Will Return an error if the task or poject creation fails
+fn handle_add(args: flags::AddArgs, app: toado::Server) -> Result<Option<String>, toado::Error> {
     if args.task || !args.project {
-        let (task_id, task_name) = create_task(args, app)?;
+        let (task_id, task_name) = commands::create_task(args, app)?;
         Ok(Some(format!("Created task {task_name} with id {task_id}")))
     } else {
         Err(Into::into("project adding not implemented"))
-    }
-}
-
-fn create_task(args: flags::AddArgs, app: toado::Server) -> Result<(i64, String), toado::Error> {
-    let theme = dialoguer::theme::ColorfulTheme::default();
-
-    let name = option_or_input(
-        args.name,
-        dialoguer::Input::with_theme(&theme).with_prompt("Name"),
-    )?;
-
-    let priority = option_or_input(
-        args.item_priority,
-        dialoguer::Input::with_theme(&theme).with_prompt("Priority"),
-    )?;
-
-    let start_time = if args.optional {
-        None
-    } else {
-        option_or_input_option(
-            args.start_time,
-            dialoguer::Input::with_theme(&theme)
-                .with_prompt("Start Time (optional)")
-                .allow_empty(true),
-        )?
-    };
-
-    let end_time = if args.optional {
-        None
-    } else {
-        option_or_input_option(
-            args.end_time,
-            dialoguer::Input::with_theme(&theme)
-                .with_prompt("End Time (optional)")
-                .allow_empty(true),
-        )?
-    };
-
-    let repeat = if args.optional {
-        None
-    } else {
-        option_or_input_option(
-            args.repeat,
-            dialoguer::Input::with_theme(&theme)
-                .with_prompt("Repeats (optional)")
-                .allow_empty(true),
-        )?
-    };
-
-    let task_id = app.add_task(toado::AddTaskArgs {
-        name: String::from(&name),
-        priority,
-        status: toado::ItemStatus::Incomplete,
-        start_time,
-        end_time,
-        repeat,
-        notes: None,
-    })?;
-
-    Ok((task_id, name))
-}
-
-/// Return the `T` of an `Option<T>` if `Option<T>` is `Some<T>`, otherwise, prompt the user for an
-/// input of type `T`.
-///
-/// # Errors
-///
-/// Returns error if getting user input fails
-fn option_or_input<T>(value: Option<T>, input: dialoguer::Input<T>) -> Result<T, toado::Error>
-where
-    T: Clone + ToString + std::str::FromStr,
-    <T as std::str::FromStr>::Err: ToString,
-{
-    match value {
-        Some(value) => Ok(value),
-        None => Ok(input.interact_text()?),
-    }
-}
-
-/// Return the `Some(String)` of an `Option<String>` if `Option<String>` is `Some(String)`, otherwise,
-/// prompt the user for an input of type `String`. If user input is blank, return `None`
-/// TODO: Make this function generic. ie. `Result<Option<T>, toado::Error>`
-///
-/// # Errors
-///
-/// Returns error if getting user input fails
-fn option_or_input_option(
-    value: Option<String>,
-    input: dialoguer::Input<String>,
-) -> Result<Option<String>, toado::Error> {
-    match value {
-        Some(value) => Ok(Some(value)),
-        None => {
-            let user_input = input.interact_text()?;
-
-            Ok(if !user_input.is_empty() {
-                Some(user_input)
-            } else {
-                None
-            })
-        }
     }
 }
